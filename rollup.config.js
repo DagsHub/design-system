08b24d5e59288625f7219e19fa7a894a9b84340b
Guy Peer <guypeer8@gmail.com>
2023-01-30 13:10:23 +0200

    fix rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+const dts = require("rollup-plugin-dts").default;
+const scss = require("rollup-plugin-scss");
 const postcss = require("rollup-plugin-postcss");
 const commonjs = require("@rollup/plugin-commonjs");
 const resolve = require("@rollup/plugin-node-resolve");
@@ -24,7 +26,14 @@ module.exports = [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      scss({ sourceMap: true, outputStyle: 'compressed' }),
       postcss(),
     ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+    external: [/\.(css|less|scss)$/],
   },
 ];
