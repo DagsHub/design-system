a99d4c13aa0de68b2ce5db6fd587c645a6d4c27a
Guy Peer <guypeer8@gmail.com>
2023-01-31 18:10:55 +0200

    menu

--- src/components/dagshub/organization/menus/horizontal-menu-tab/index.tsx
+++ src/components/dagshub/organization/menus/horizontal-menu-tab/index.tsx
@@ -1,20 +1,20 @@
+import React from 'react';
+import { IconProps } from "../../../../icons";
+
 import './horizontal-menu-tab.scss';
 import '../../../../styles/root.scss';
-import React from 'react';
-import {IconProps} from "../../../../icons";
-
 export interface HorizontalMenuTabProps{
     text: string;
     icon?: React.ReactElement<IconProps>;
     count?: number;
 }
 
-export function HorizontalMenuTab(props:HorizontalMenuTabProps) {
+export function HorizontalMenuTab({ text, icon, count } :HorizontalMenuTabProps) {
     return (
         <button type="button" className="horizontal-menu-tab">
-            {props.icon && <div>{props.icon}</div>}
-            {props.text}
-            {props.count && <div className={"horizontal-menu-tab__counter"}>{props.count}</div>}
+            {icon && <div>{icon}</div>}
+            {text}
+            {count && <div className={"horizontal-menu-tab__counter"}>{count}</div>}
         </button>
     )
 }
