1ab5893537d6c9598d66c906bbce3ba3920f19ad
Guy Peer <guypeer8@gmail.com>
2023-01-31 18:14:42 +0200

    a

---
+++ src/stories/dagshub/organization/header/OrganizationHeader.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import type {Meta, StoryFn } from '@storybook/react';
+import {Header, HeaderProps} from '../../../../components/dagshub/organization/header';
+
+const meta: Meta<HeaderProps> = {
+    title: 'DagsHub/Org/Header/Header',
+    component: Header
+};
+
+export default meta;
+
+const Template: StoryFn<HeaderProps> = args => <Header {...args} />;
+
+const imageLink="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH3HZUQ9Uz5qcOaIwRjQi0jdfJVVUIR-hO9Q&usqp=CAU"
+
+export const Primary = Template.bind({});
+Primary.args = {
+    orgImageSource: imageLink,
+    orgName:"Dagshub",
+    orgSite:"https://DagsHub.com",
+    orgPlan:"teams plan"
+};
