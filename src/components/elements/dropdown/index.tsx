333186ef661f4df7aa191932c99ae2c641d559ba
Guy Peer <guypeer8@gmail.com>
2023-02-13 12:06:45 +0200

    add basic dropdown

--- src/components/elements/dropdown/index.tsx
+++ src/components/elements/dropdown/index.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Icon } from '../../icons';
 import { RadioButtonItemProps, RadioButtonList } from '../../forms';
 
 import '../../styles/root.scss';
 import './dropdown.scss';
+import classNames from 'classnames';
 
 export interface DropdownOption {
   id: string;
@@ -12,6 +13,7 @@ export interface DropdownOption {
 }
 
 export interface DropdownProps {
+  kind?: 'basic' | 'radio';
   width: number;
   label: string;
   isCollapsed?: boolean;
@@ -20,17 +22,28 @@ export interface DropdownProps {
 }
 
 export const Dropdown = ({
+  kind = 'basic',
   width,
   label,
   options = [],
   onItemChecked = () => {},
   ...props
 }: DropdownProps & React.ButtonHTMLAttributes<HTMLDivElement>) => {
+  const [checked, setChecked] = useState<number | string>('');
   const [isCollapsed, setIsCollapsed] = useState<boolean>(true);
+
+  const _options = options.map(opt => ({ ...opt, checked: opt.id === checked }));
+  const checkedOptLabel = _options.find(opt => opt.checked)?.label;
+
+  useEffect(function onChecked() {
+    onItemChecked(checked);
+    setIsCollapsed(true);
+  }, [checked]);
+
   return (
     <div className="dagshub-dropdown" style={{ width }} {...props}>
       <div className="dagshub-dropdown__box" onClick={() => setIsCollapsed(!isCollapsed)}>
-        {label}
+        {checkedOptLabel || label}
         <Icon
           width={10}
           height={6}
@@ -38,12 +51,25 @@ export const Dropdown = ({
           icon={`solid-cheveron-${isCollapsed ? 'down' : 'up'}`}
         />
       </div>
-      {!isCollapsed && (
+      {kind === 'radio' && !isCollapsed && (
         <RadioButtonList
-          className="dagshub-dropdown__options"
-          items={options}
-          onChecked={onItemChecked}
+          items={_options}
+          onChecked={setChecked}
+          className="dagshub-dropdown__options dagshub-dropdown__options-radio"
         />
+      )}  
+      {kind === 'basic' && !isCollapsed && (
+         <div className='dagshub-dropdown__options'>
+          {_options?.map((opt: RadioButtonItemProps) => (
+            <div 
+              key={opt.id} 
+              onClick={() => setChecked(opt.id)} 
+              className={classNames('dagshub-dropdown__options-opt', { checked: opt.checked })}
+            >
+              {opt.label}
+            </div>
+         ))}
+       </div>
       )}
     </div>
   );
--- src/stories/elements/dropdown/dropdown.stories.tsx
+++ src/stories/elements/dropdown/dropdown.stories.tsx
@@ -11,8 +11,8 @@ export default meta;
 
 const Template: StoryFn<DropdownProps> = (args) => <Dropdown {...args} />;
 
-export const Collapsed = Template.bind({});
-Collapsed.args = {
+export const OpenedBasic = Template.bind({});
+OpenedBasic.args = {
   width: 350,
   label: 'Country',
   options: [
@@ -22,9 +22,22 @@ Collapsed.args = {
   ]
 };
 
-export const Opened = Template.bind({});
-Opened.args = {
+export const CollapsedRadio = Template.bind({});
+CollapsedRadio.args = {
   width: 350,
+  kind: 'radio',
+  label: 'Country',
+  options: [
+    { id: 1, label: 'Israel', description: 'State of Israel - Middle East - Asia', checked: true },
+    { id: 2, label: 'Britain', description: 'Great British Empire - Europe' },
+    { id: 3, label: 'Colombia', description: 'La Tierra de Oro - South America' }
+  ]
+};
+
+export const OpenedRadio = Template.bind({});
+OpenedRadio.args = {
+  width: 350,
+  kind: 'radio',
   label: 'Country',
   isCollapsed: false,
   options: [
