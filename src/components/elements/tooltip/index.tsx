79e393ccff9e988360a22d149cb4c208147464b9
Guy Peer <guypeer8@gmail.com>
2023-02-27 20:45:38 +0200

    add toast

--- src/components/elements/tooltip/index.tsx
+++ src/components/elements/tooltip/index.tsx
@@ -3,12 +3,12 @@ import Tippy, { TippyProps } from '@tippyjs/react';
 
 import './tooltip.scss';
 
-export interface TooltipProps {
+export type TooltipProps = {
   content: React.ReactNode | string;
   children?: React.ReactElement<any>;
-}
+} & TippyProps;
 
-export const Tooltip = ({ content, children, ...props }: TooltipProps & TippyProps) => {
+export const Tooltip = ({ content, children, ...props }: TooltipProps) => {
   return (
     <Tippy content={content} {...props}>
       {children}
