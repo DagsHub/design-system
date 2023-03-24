0e23dcf794bd453ffac6d64f5cfbe4bf1874ab05
Guy Peer <guy.peer@foretellix.com>
2023-03-24 18:52:53 +0300

    fmt

--- src/components/dagshub/data-engine/react-table/columns.d.ts
+++ src/components/dagshub/data-engine/react-table/columns.d.ts
@@ -1,26 +1,26 @@
 export const COLUMNS = [
-    {
-        Header: 'Id',
-        accessor: 'id'
-    },
-    {
-        Header: 'First Name',
-        accessor: 'first_name'
-    },
-    {
-        Header: 'Last Name',
-        accessor: 'last_name'
-    },
-    {
-        Header: 'Date Of Birth',
-        accessor: 'date_of_birth'
-    },
-    {
-        Header: 'Country',
-        accessor: 'country'
-    },
-    {
-        Header: 'Phone',
-        accessor: 'phone'
-    }
-]
+  {
+    Header: 'Id',
+    accessor: 'id'
+  },
+  {
+    Header: 'First Name',
+    accessor: 'first_name'
+  },
+  {
+    Header: 'Last Name',
+    accessor: 'last_name'
+  },
+  {
+    Header: 'Date Of Birth',
+    accessor: 'date_of_birth'
+  },
+  {
+    Header: 'Country',
+    accessor: 'country'
+  },
+  {
+    Header: 'Phone',
+    accessor: 'phone'
+  }
+];
