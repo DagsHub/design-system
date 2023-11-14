5db719ad830a97bf9ed6982a4ce1ad717d44354e
Tal Malka <tal@dagshub.com>
2023-11-14 10:36:59 +0200

    export components

--- src/components/dagshub/data-engine/index.ts
+++ src/components/dagshub/data-engine/index.ts
@@ -2,3 +2,5 @@ export * from './dataset-settings-modal';
 export * from './react-table';
 export * from './generic-card';
 export * from './file-tree';
+export * from './singleFileViewModal/ItemFallback';
+export * from './singleFileViewModal/SingleFileViewModal';
