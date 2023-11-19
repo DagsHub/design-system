29ef0f3dc08a01c14d9e6b37ab5d7d0429cf9188
Tal Malka <tal@dagshub.com>
2023-11-19 14:10:21 +0200

    add stories to customTextField component

---
+++ src/stories/dagshub/data-engine/customTextField.stories.tsx/customTextField.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+import CustomTextField from "../../../../components/dagshub/data-engine/metadataKeyValue/CustomTextField";
+
+const meta: Meta<typeof CustomTextField> = {
+  title: 'DagsHub/Data-Engine/CustomTextField/Custom text field',
+  component: CustomTextField
+};
+
+export default meta;
+
+const Template: StoryFn<typeof CustomTextField> = (args) => <CustomTextField {...args} />;
+
+export const customTextFieldStoriesReadOnly: StoryFn<typeof CustomTextField> = Template.bind({});
+customTextFieldStoriesReadOnly.args = {
+  readOnly:true,
+  value:"bla",
+};
+export const customTextFieldStories: StoryFn<typeof CustomTextField> = Template.bind({});
+customTextFieldStories.args = {
+  readOnly:false,
+  placeholder: 'Enter value...'
+};
