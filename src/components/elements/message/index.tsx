95c041d64bc93ce7fb386e7f428d5a693e6630ad
Ido Nov <idonov8@gmail.com>
2024-01-14 13:29:41 +0200

    Avoid CSS file and use mui component as a base

--- src/components/elements/message/index.tsx
+++ src/components/elements/message/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import './index.scss';
-import { Icon } from '../../icons';
-import classNames from "classnames";
-import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
 
 export interface MessageProps {
     text: string;
@@ -12,35 +10,35 @@ export interface MessageProps {
 
 export function Message({ text, kind, support }: MessageProps) {
     return (
-        <div className={classNames('message-component', kind)}>
-            <div className={'message-component__icon'}>
-                {kind === 'info' ? (
-                    <Icon icon={'outline-information-circle'} width={20} height={20} fill={'#2563EB'}/>
-                ) : kind === 'warning' ? (
-                    <Icon icon={'outline-exclamation-circle'} width={20} height={20} fill={'#F97316'}/>
-                ) : kind === 'error' ? (
-                    <Icon icon={'outline-exclamation-circle'} width={20} height={20} fill={'#DC2626'}/>
-                ) : kind == 'success' ? (
-                    <CheckCircleOutlineIcon sx={{color: '#059669'}} />
-                ) : null
-                }
-            </div>
-            <div className={'message-component__text'}>
-                <div className={'message-component__title'}>{text}</div>
-                {support && (
-                    <div className={'message-component__support'}>
-                        Contact us via our{' '}
-                        <a
-                            className={'message-component__support-link'}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            href={'https://discord.com/invite/9gU36Y6'}
-                        >
-                            support channel
-                        </a>
-                    </div>
-                )}
-            </div>
-        </div>
+        <Alert severity={kind} sx={
+            kind === 'info' ? ({
+                background: '#eff6ff',
+                border: '1px solid #93c5fd',
+            }) : kind === 'warning' ? ({
+                background: '#fefbea',
+                border: '1px solid #fde68a',
+            }) : kind === 'error' ? ({
+                background: '#fef2f2',
+                border: '1px solid #fecaca',
+            }) : kind == 'success' ? ({
+                background: '#f0fdf4',
+                border: '1px solid #86efac',
+            }) : null
+        }>
+            {support ? (
+                <>
+                    <AlertTitle>{text}</AlertTitle>
+                    Contact us via our{' '}
+                    <a
+                        className={'message-component__support-link'}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href={'https://discord.com/invite/9gU36Y6'}
+                    >
+                        support channel
+                    </a>
+                </>
+            ) : text}
+        </Alert>
     );
 }
