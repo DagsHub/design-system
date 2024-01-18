79163ec0782d8b17f78d7cc91bea49645f916370
Tal Malka <tal@dagshub.com>
2024-01-18 17:35:14 +0200

    fix conversion logic

--- src/components/dagshub/data-engine/queryBuilder/QueryBuilderContext.tsx
+++ src/components/dagshub/data-engine/queryBuilder/QueryBuilderContext.tsx
@@ -192,7 +192,9 @@ export const QueryBuilderProvider = ({
   }, [metadataFields]);
 
   useEffect(() => {
-    onChange({ ...queryInput, query: rootCondition });
+    onChange({ ...queryInput, query: convertUiFormatToBackandFormat(
+        removeIdFields(rootCondition ?? {})
+      ) ?? undefined, });
   }, [rootCondition]);
 
   function generateUniqueId(): string {
@@ -238,6 +240,137 @@ export const QueryBuilderProvider = ({
       default:
         return true;
     }
+  }
+
+  function getZeroValueByType(type: MetadataType | undefined): string {
+    switch (type) {
+      case 'BOOLEAN':
+        return 'false';
+      case 'INTEGER':
+        return '0';
+      case 'FLOAT':
+        return '0.0';
+      case 'STRING':
+        return '';
+      case 'BLOB':
+        return '';
+      default:
+        return '';
+    }
+  }
+
+  function removeIdFields(input: QueryInput | AndOrMetadataInput | MetadataInput): any {
+    if (Array.isArray(input)) {
+      return input.map((item) => removeIdFields(item));
+    } else if (typeof input === 'object' && input !== null) {
+      if ('id' in input) {
+        const { id, ...rest } = input;
+        const result: any = { ...rest };
+        for (const key in result) {
+          result[key] = removeIdFields(result[key]);
+        }
+        return result;
+      } else {
+        const result: any = { ...input };
+        for (const key in result) {
+          result[key] = removeIdFields(result[key]);
+        }
+        return result;
+      }
+    }
+    return input;
+  }
+
+  function convertUiFormatToBackandFormat(
+    condition: AndOrMetadataInput
+  ): AndOrMetadataInput | null {
+    if (!!condition.or || !!condition.and) {
+      // Recursively convert to backand format
+      const formattedNestedConditions = (condition.or || condition.and || [])
+        .flatMap((c) => convertUiFormatToBackandFormat(c) as AndOrMetadataInput);
+
+      // Return the modified condition
+      if (condition.or) {
+        return { or: formattedNestedConditions };
+      } else {
+        return { and: formattedNestedConditions };
+      }
+    }
+    if ((condition.filter?.comparator as string) === 'IS_POSITIVE_INFINITY') {
+      return {
+        ...condition,
+        filter: { ...condition.filter, comparator: 'EQUAL', value: '+Inf' },
+      };
+    }
+    if ((condition.filter?.comparator as string) === 'IS_NEGATIVE_INFINITY') {
+      return {
+        ...condition,
+        filter: { ...condition.filter, comparator: 'EQUAL', value: '-Inf' },
+      };
+    }
+    if ((condition.filter?.comparator as string) === 'IS_NAN') {
+      return {
+        ...condition,
+        filter: { ...condition.filter, comparator: 'EQUAL', value: 'NaN' },
+      };
+    }
+    if (condition.filter?.comparator === 'IS_NULL') {
+      return {
+        ...condition,
+        filter: {
+          ...condition.filter,
+          value: getZeroValueByType(condition.filter.valueType),
+        },
+      };
+    }
+    return condition;
+  }
+
+  function convertBackandFormatToUiFormat(
+    condition: AndOrMetadataInput
+  ): AndOrMetadataInput | null {
+    if (!!condition.or || !!condition.and) {
+      // Recursively convert to ui format
+      const formattedNestedConditions = (condition.or || condition.and || [])
+        .flatMap((c) => convertBackandFormatToUiFormat(c) as AndOrMetadataInput);
+
+      // Return the modified condition
+      if (condition.or) {
+        return { or: formattedNestedConditions };
+      } else {
+        return { and: formattedNestedConditions };
+      }
+    }
+    if(condition?.filter?.valueType === "FLOAT" && condition?.filter?.comparator === "EQUAL"){
+      if (condition.filter.value === '+Inf') {
+        return {
+          ...condition,
+          filter: { ...condition.filter, comparator: 'IS_POSITIVE_INFINITY', value: '' },
+        };
+      }
+      if (condition.filter.value === '-Inf') {
+        return {
+          ...condition,
+          filter: { ...condition.filter, comparator: 'IS_NEGATIVE_INFINITY', value: '' },
+        };
+      }
+      if (condition.filter.value === 'NaN') {
+        return {
+          ...condition,
+          filter: { ...condition.filter, comparator: 'IS_NAN', value: '' },
+        };
+      }
+    }
+    if (condition.filter?.comparator === 'IS_NULL') {
+      return {
+        ...condition,
+        filter: {
+          ...condition.filter,
+          value: '',
+        },
+      };
+    }
+    return condition;
   }
 
   return (
